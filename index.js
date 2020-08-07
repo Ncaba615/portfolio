1547a6455dcf909d7221344abbf2ab15d43656d5
Nigel <Ncaba615@gmail.com>
2020-08-06 17:34:39 -0700

    animations

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(window).scroll(function () {
-  if ($(this).scrollTop() > 1075) {
+  if ($(this).scrollTop() > 900) {
     $("#dynamic").addClass("animate__animated animate__bounce");
   } else {
     $("#dynamic").removeClass("animate__animated animate__bounce");
@@ -7,9 +7,20 @@ $(window).scroll(function () {
 });
 
 $(window).scroll(function () {
-  if ($(this).scrollTop() > 1075) {
+  if ($(this).scrollTop() > 900) {
     $("#sup").addClass("animate__animated animate__bounce");
   } else {
     $("#sup").removeClass("animate__animated animate__bounce");
   }
 });
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 600) {
+    $("#places")
+      .addClass("animate__animated animate__fadeIn")
+      .removeClass("invisible");
+  }
+});
+
+
+
